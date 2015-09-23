e2b6a15dfa3b74acdf5855a0a5af6f45c6829eaa
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2015-09-23 22:18:43 +0200

    added recipe basic markup

--- CookbookWin10/RecipePage.xaml.cs
+++ CookbookWin10/RecipePage.xaml.cs
@@ -5,32 +5,32 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CookbookWin10
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RecipePage : Page
     {
-        public MainPage()
+        public RecipePage()
         {
             this.InitializeComponent();
-        }
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
 
-        private void buttonClick(object sender, RoutedEventArgs e)
-        {
-            //greetingOutput.Text = "Hello " + nameInput.Text + "!";
-            this.Frame.Navigate(typeof(RecipePage), null);           
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri("ms-appx:///Assets/Logo.png"));
         }
     }
 }
